8a4430a1287abebf20a47020f88abf56c5ca1b2c
yc9z <51027990+yc9z@users.noreply.github.com>
2019-06-17 15:15:25 -0500

    Update ANIMAtch.py

--- ANIMAtch.py
+++ ANIMAtch.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-"""
-This simple example is used for the line-by-line tutorial
-that comes with pygame. It is based on a 'popular' web banner.
-Note there are comments here, but for the full explanation,
-follow along in the tutorial.
-"""
-
 
 #Import Modules
 import os, pygame
